fd8e791557f809159bcb485a21bee200a588cc66
mollymeissner <mollymeissner@gmail.com>
2019-11-20 17:10:03 -0800

    to see changes

---
+++ add-content.js
@@ -0,0 +1,18 @@
+
+var responseName = prompt('What is your name?');
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon';
+} else if (hourNow > 0) {
+    greeting = 'Good morning';
+} else {
+    greeting = 'Happy November';
+}
+
+document.write('<h3>' + greeting + ' ' + '</h3>');
